241c857de7e078c38260f104df3afc03bc6f7906
muhammeta7 <muhammeta7@gmail.com>
2020-04-28 23:55:34 -0400

    semi-complete about me portion of things

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import Navigation from "./components/navbar/Navigation";
 import { BrowserRouter as Router } from "react-router-dom";
+import AboutMe from "./components/about-me/AboutMe";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <AboutMe />
       </div>
     </Router>
   );
